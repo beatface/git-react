21cd3fb6a7522af2e10712b8a2001a08f02ce333
beatface <emma.beaton@gmail.com>
2016-04-04 11:52:33 -0500

    added first component

---
+++ app/components/Home.js
@@ -0,0 +1,13 @@
+const React = require('react')
+
+const Home = React.createClass({
+    render: function() {
+        return (
+            <h2 className='text-center'>
+                Search by GitHub username
+            </h2>
+        )
+    }
+})
+
+module.exports = Home
---
+++ app/components/Main.js
@@ -0,0 +1,14 @@
+const React = require('react')
+// const Home = require('../components/Home')
+
+const Main = React.createClass({
+    render: function() {
+        return (
+            <div>
+                <h1>Hello!</h1>
+            </div>
+        )
+    }
+})
+
+module.exports = Main
