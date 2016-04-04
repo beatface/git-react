94f839d66ca7be381d06b75f62a1c5fa2672195e
beatface <emma.beaton@gmail.com>
2016-04-04 11:26:13 -0500

    added basic starter files - webpack.config, app.js, etc.

---
+++ app/config/routes.js
@@ -0,0 +1,11 @@
+const React = require('react')
+const Main = require('../components/Main')
+const Router = require('react-router')
+const Route = Router.Route
+const IndexRoute = Router.IndexRoute
+
+module.exports = (
+    <Route path='/' component={Main}>
+        <IndexRoute component={List}/>
+    </Route>
+)
